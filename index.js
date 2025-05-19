941f3f838d4298eadb38c3195879bd18bea6d32c
TheAnalyticsCareer <careeranalytics499@gmail.com>
2025-05-19 13:29:43 +0530

    update

--- index.js
+++ index.js
@@ -35,13 +35,13 @@ const limiter = rateLimit({
 //   credentials: true
 // }));
 
-// app.use(cors());
+app.use(cors());
 
-app.use(cors({
-  origin: ['https://aidf-home-interior-service.vercel.app', 'http://localhost:3000'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: ['https://aidf-home-interior-service.vercel.app', 'http://localhost:3000'],
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   credentials: true
+// }));
 
 
 
